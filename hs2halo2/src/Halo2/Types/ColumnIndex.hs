1f5d82a74a8cbcfdefe9e3e379e6601d2e52d10d
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-12-24 15:02:14 -0500

    column index

---
+++ hs2halo2/src/Halo2/Types/ColumnIndex.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Halo2.Types.ColumnIndex
+  ( ColumnIndex (ColumnIndex, getColumnIndex)
+  ) where
+
+
+import Halo2.Prelude
+
+
+newtype ColumnIndex = ColumnIndex { getColumnIndex :: Int }
+  deriving (Eq, Ord, Num, Enum, Show, Generic)
