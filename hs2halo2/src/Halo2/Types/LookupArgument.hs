327ca1ff55f5f8c6f1df524a2920f7ca328a0d5e
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-12-24 15:35:40 -0500

    lookup argument stub

---
+++ hs2halo2/src/Halo2/Types/LookupArgument.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module Halo2.Types.LookupArgument ( LookupArgument ) where
+
+
+data LookupArgument
+  -- TODO LookupArgument
