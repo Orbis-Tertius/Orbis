a0cc6d0691c14eb22b7d86337b3ec5b161432804
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-12-28 21:25:13 -0500

    wip integers

--- uplc2c/rts/rts.c
+++ uplc2c/rts/rts.c
@@ -41,14 +41,56 @@ void *alloc(int bytes) {
  * Integers
  *****************************************************************************/
 
-// Integers are unlimited in size. We represent them as linked lists of words,
-// least significant word first, with the sign being stored in the most
-// significant word.
+// Integers are unlimited in size. We store the sign of an integer
+// in the type tag. The rest of the integer is two words to represent a natural.
+// Natural numbers are represented as linked lists of words,
+// least significant word first.
+
+struct Natural {
+  int less_significant; // must be non-negative
+  struct Natural *more_significant; // null if this is the most significant word.
+};
+
 
 struct Integer {
-  int less_significant;
-  struct Integer *more_significant; // null if this is the most significant word.
+  int sign;
+  Natural nat;
 };
+
+
+// Adds a to b, destructively updating b.
+void add_nat (Natural *a, Natural *b) {
+  int a_ls = a->less_significant;
+  int b_ls = b->less_significant;
+  int carry = 0;
+  int d = a_ls + b_ls;
+  if (d < 0) {
+    carry = 1;
+    d = (a_ls - MAX_INT) + b_ls;
+  }
+  b->less_significant = d;
+  struct Natural *a_ms = a->more_significant;
+  struct Natural *b_ms = b->more_significant;
+  if (a_ms || b_ms || carry) {
+    if (!b_ms) {
+      b_ms = (Natural *)alloc(sizeof(Natural));
+      b_ms->less_significant = 0;
+      b_ms->more_significant = 0;
+      b->more_significant = b_ms;
+    }
+
+    if (carry) {
+      struct Natural c;
+      c.less_significant = carry;
+      c.more_significant = 0;
+      add_nat (&c, b_ms);
+    }
+
+    if (a_ms) {
+      add_nat (a_ms, b_ms);
+    }
+  }
+}
 
 
 /*****************************************************************************
@@ -60,7 +102,8 @@ struct Integer {
 
 enum NFDataType {
     FunctionType
-  , IntegerType
+  , IntegerPositive
+  , IntegerNegative
   , UnitType
   , BoolType
   , ThunkType // (result of Delay)
