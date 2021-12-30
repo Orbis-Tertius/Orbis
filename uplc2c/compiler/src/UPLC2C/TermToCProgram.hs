491ff68953f8d14833406c71aef1627d47333d52
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-12-30 16:07:50 -0500

    termToCProgram Var case

--- uplc2c/compiler/src/UPLC2C/TermToCProgram.hs
+++ uplc2c/compiler/src/UPLC2C/TermToCProgram.hs
@@ -1,16 +1,29 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 
 module UPLC2C.TermToCProgram ( termToCProgram ) where
 
 
+import Data.Text (pack)
+import PlutusCore.DeBruijn (DeBruijn (..), Index (..))
+import UntypedPlutusCore.Core.Type (Term (Var))
+
 import UPLC2C.Prelude
-import UPLC2C.Types.CProgram (CProgram (..))
+import UPLC2C.Types.CFunctionDefinition (CFunctionDefinition (..))
+import UPLC2C.Types.CName (CName (..))
 import UPLC2C.Types.CProgramBuilder (CProgramBuilder (..))
+import UPLC2C.Types.DeBruijnIndex (DeBruijnIndex (..))
 import UPLC2C.Types.UPLCTerm (UPLCTerm)
 
 
-termToCProgram :: ( Monad m, CProgramBuilder m ) => UPLCTerm -> m CProgram
-termToCProgram _ = do
-  return () -- TODO
-  getProgram
+termToCProgram :: ( Monad m, CProgramBuilder m ) => UPLCTerm -> m ()
+termToCProgram =
+  \case
+    Var _ ix@(DeBruijn (Index i)) ->
+      addToProgram (deBruijnIndexToCName ix) (VariableReference (DeBruijnIndex (fromIntegral i)))
+
+
+deBruijnIndexToCName :: DeBruijn -> CName
+deBruijnIndexToCName (DeBruijn i) = CName $ "lookup_var_" <> pack (show i)
