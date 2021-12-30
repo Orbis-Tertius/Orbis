5d18bfcbf3a3277833748541bff024115390f19d
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-12-30 16:15:45 -0500

    wip termToCProgram

--- uplc2c/compiler/src/UPLC2C/TermToCProgram.hs
+++ uplc2c/compiler/src/UPLC2C/TermToCProgram.hs
@@ -8,7 +8,7 @@ module UPLC2C.TermToCProgram ( termToCProgram ) where
 
 import Data.Text (pack)
 import PlutusCore.DeBruijn (DeBruijn (..), Index (..))
-import UntypedPlutusCore.Core.Type (Term (Var))
+import qualified UntypedPlutusCore.Core.Type as UPLC
 
 import UPLC2C.Prelude
 import UPLC2C.Types.CFunctionDefinition (CFunctionDefinition (..))
@@ -18,12 +18,42 @@ import UPLC2C.Types.DeBruijnIndex (DeBruijnIndex (..))
 import UPLC2C.Types.UPLCTerm (UPLCTerm)
 
 
-termToCProgram :: ( Monad m, CProgramBuilder m ) => UPLCTerm -> m ()
+termToCProgram :: ( Monad m, CProgramBuilder m ) => UPLCTerm -> m CName
 termToCProgram =
   \case
-    Var _ ix@(DeBruijn (Index i)) ->
-      addToProgram (deBruijnIndexToCName ix) (VariableReference (DeBruijnIndex (fromIntegral i)))
+    UPLC.Var _ ix@(DeBruijn (Index i)) -> do
+      let name = deBruijnIndexToCName ix
+      addToProgram name (VariableReference (DeBruijnIndex (fromIntegral i)))
+      return name
+    UPLC.LamAbs _ _ subterm -> do
+      subtermName <- termToCProgram subterm
+      absName <- genSym
+      addToProgram absName (CreateClosureOver subtermName)
+      return absName
+    UPLC.Apply _ operator operand -> do
+      operatorName <- termToCProgram operator
+      operandName <- termToCProgram operand
+      applyName <- genSym
+      addToProgram applyName (Apply operatorName operandName)
+      return applyName
+    UPLC.Force _ operand -> do
+      operandName <- termToCProgram operand
+      forceName <- genSym
+      addToProgram forceName (Force operandName)
+      return forceName
+    UPLC.Delay _ operand -> do
+      operandName <- termToCProgram operand
+      delayName <- genSym
+      addToProgram delayName (Delay operandName)
+      return delayName
+    UPLC.Constant _ _ -> todo
+    UPLC.Builtin _ _ -> todo
+    UPLC.Error _ -> return (CName "builtin_error")
 
 
 deBruijnIndexToCName :: DeBruijn -> CName
 deBruijnIndexToCName (DeBruijn i) = CName $ "lookup_var_" <> pack (show i)
+
+
+todo :: a
+todo = todo
