6f7faacf6bcd390c1590857604946fe199e9f9a9
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-12-30 03:01:00 -0500

    CCode, stub compileFunctionDefinition

---
+++ uplc2c/compiler/src/UPLC2C/CompileFunctionDefinition.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module UPLC2C.CompileFunctionDefinition ( compileFunctionDefinition ) where
+
+
+--import UPLC2C.Prelude
+import UPLC2C.Types.CCode (CCode)
+import UPLC2C.Types.CFunctionDefinition (CFunctionDefinition)
+import UPLC2C.Types.CName (CName)
+
+
+compileFunctionDefinition :: CName -> CFunctionDefinition -> CCode
+compileFunctionDefinition = todo
+
+
+todo :: a
+todo = todo
---
+++ uplc2c/compiler/src/UPLC2C/Types/CCode.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+
+module UPLC2C.Types.CCode ( CCode (..) ) where
+
+
+import UPLC2C.Prelude
+
+
+newtype CCode = CCode { unCCode :: Text }
+  deriving (Eq, Ord, Show)
