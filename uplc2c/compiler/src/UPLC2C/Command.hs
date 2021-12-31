62ab8e22899b843781fdf140bd3188c07e3fe97e
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-12-30 20:49:48 -0500

    command parser

--- uplc2c/compiler/src/UPLC2C/Command.hs
+++ uplc2c/compiler/src/UPLC2C/Command.hs
@@ -7,6 +7,7 @@ module UPLC2C.Command ( main ) where
 import Codec.Serialise (deserialiseOrFail)
 import Data.ByteString.Lazy (readFile)
 import Data.Text.IO (writeFile)
+import qualified Options.Applicative as O
 import Plutus.V1.Ledger.Scripts (Script (..))
 import qualified UntypedPlutusCore.Core.Type as UPLC
 
@@ -25,11 +26,30 @@ main = runCommand =<< parseCommand
 
 
 parseCommand :: MonadIO m => m Command
-parseCommand = liftIO todo
+parseCommand = liftIO $ O.execParser commandInfo
 
 
-todo :: a
-todo = todo
+inputFilePath :: O.Parser InputFilePath
+inputFilePath =
+  O.argument (InputFilePath <$> O.str) (O.metavar "INPUT" <> O.help "The input file path")
+
+
+outputFilePath :: O.Parser OutputFilePath
+outputFilePath =
+  O.argument (OutputFilePath <$> O.str) (O.metavar "OUTPUT" <> O.help "The output file path")
+
+
+command :: O.Parser Command
+command = CompileFile <$> inputFilePath <*> outputFilePath
+
+
+commandInfo :: O.ParserInfo Command
+commandInfo =
+  O.info (command O.<**> O.helper)
+    ( O.fullDesc
+   <> O.progDesc "Compile Plutus bytecode to C"
+   <> O.header "uplc2c - Untyped Plutus Core to C"
+    )
 
 
 runCommand :: MonadIO m => Command -> m ()
